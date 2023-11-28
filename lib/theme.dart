fec60e6610737b374baf6de4fd89bca843f5f9b2
Théo K <cybertheo37@gmail.com>
2023-11-28 21:27:51 +0100

    feat: change app theme color

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   // F0DCED to MaterialColor
+  // http://mcg.mbitson.com/#!?mcgpalette0=%23ff414d
   static final Map<int, Color> _color = {
     50: const Color.fromRGBO(240, 220, 237, .1),
     100: const Color.fromRGBO(240, 220, 237, .2),
@@ -12,10 +13,10 @@ class AppTheme {
     600: const Color.fromRGBO(240, 220, 237, .7),
     700: const Color.fromRGBO(240, 220, 237, .8),
     800: const Color.fromRGBO(240, 220, 237, .9),
-    900: const Color.fromARGB(255, 231, 175, 223),
+    900: const Color.fromARGB(255, 255, 28, 37),
   };
 
-  MaterialColor mainCustomColor = MaterialColor(0xFFF1A8E6, _color);
+  MaterialColor mainCustomColor = MaterialColor(0xFFFF414D, _color);
 
   ThemeData get() {
     return ThemeData(
